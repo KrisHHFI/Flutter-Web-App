60d927e8808f770d537f546bd5d19971cf632036
KrisHHFI <kristopherpepperuk@gmail.com>
2024-09-09 21:15:30 +0300

    Single text widget small screen dimensions

--- lib/widgets/single_text.dart
+++ lib/widgets/single_text.dart
@@ -12,16 +12,29 @@ class SingleText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    final isSmallScreen = screenSize.width <= 600;
+
     return SizedBox(
-      width: 500,
-      child: Center(
-        child: Text(
-          text,
-          style: TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
-            fontStyle: FontStyle.italic,
-            color: textColor, // Apply the text color
+      width:
+          isSmallScreen ? double.infinity : 500, // Full width on small screens
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal:
+              isSmallScreen ? 10 : 0.0, // 5 units padding on small screens
+        ),
+        child: Center(
+          child: Text(
+            text,
+            textAlign: isSmallScreen
+                ? TextAlign.center
+                : TextAlign.left, // Center text if small screen
+            style: TextStyle(
+              fontSize: 40, // Font size remains 40 for both screen sizes
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic,
+              color: textColor, // Apply the text color
+            ),
           ),
         ),
       ),

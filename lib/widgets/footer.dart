fa7b08acd2a2fa9abf9bc14e4bcc6ac79baed0ee
KrisHHFI <kristopherpepperuk@gmail.com>
2024-09-10 16:51:57 +0300

    Footer links working

--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Footer extends StatelessWidget {
   const Footer({super.key});
+
+  // Method to launch a URL
+  Future<void> _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,11 +20,34 @@ class Footer extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image.network('images/CameraIcon.png', width: 30, height: 30),
+          MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: GestureDetector(
+              onTap: () => _launchURL(
+                  'https://www.shutterstock.com/g/Kristopher+Pepper'), // Replace with your URL
+              child:
+                  Image.network('images/CameraIcon.png', width: 30, height: 30),
+            ),
+          ),
           const SizedBox(width: 20),
-          Image.network('images/GitHub.png', width: 30, height: 30),
+          MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: GestureDetector(
+              onTap: () => _launchURL(
+                  'https://github.com/KrisHHFI'), // Replace with your URL
+              child: Image.network('images/GitHub.png', width: 30, height: 30),
+            ),
+          ),
           const SizedBox(width: 20),
-          Image.network('images/LinkedIn.png', width: 30, height: 30),
+          MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: GestureDetector(
+              onTap: () => _launchURL(
+                  'https://www.linkedin.com/in/kristopher-pepper-824184136/'), // Replace with your URL
+              child:
+                  Image.network('images/LinkedIn.png', width: 30, height: 30),
+            ),
+          ),
         ],
       ),
     );
